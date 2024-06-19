d726919ffa62b9c5496a5df73f916392e761ac81
Ashish Chaudhari <ashishchaudharistudy1857@gmail.com>
2024-06-19 16:08:03 +0530

    iife

--- src/widget.js
+++ src/widget.js
@@ -1,41 +1,40 @@
-// widget.js
-
-export class Widget {
-    constructor({ position = 'bottom-right' } = {}) {
-      this.position = this.getPosition(position);
-      this.open = false;
-      this.initialise();
-    }
-  
-    getPosition(position) {
-      const [vertical, horizontal] = position.split('-');
-      return {
-        [vertical]: '30px',
-        [horizontal]: '30px'
-      };
-    }
-  
-    initialise() {
-      const container = document.createElement('div');
-      container.style.position = 'fixed';
-      Object.keys(this.position).forEach((key) => container.style[key] = this.position[key]);
-      document.body.appendChild(container);
-      
-      const questionElement = document.createElement('div');
-      questionElement.textContent = 'How likely are you to recommend us to a friend or colleague?';
-      container.appendChild(questionElement);
-      
-      const scaleElement = document.createElement('div');
-      for (let i = 0; i <= 10; i++) {
-        const numberElement = document.createElement('button');
-        numberElement.textContent = i;
-        numberElement.style.backgroundColor = i === 0 ? '#ff6666' : i === 10 ? '#66cc66' : '#ffffff';
-        scaleElement.appendChild(numberElement);
+(function() {
+    class Widget {
+      constructor({ position = 'bottom-right' } = {}) {
+        this.position = this.getPosition(position);
+        this.open = false;
+        this.initialise();
       }
-      container.appendChild(scaleElement);
+  
+      getPosition(position) {
+        const [vertical, horizontal] = position.split('-');
+        return {
+          [vertical]: '30px',
+          [horizontal]: '30px'
+        };
+      }
+  
+      initialise() {
+        const container = document.createElement('div');
+        container.style.position = 'fixed';
+        Object.keys(this.position).forEach((key) => container.style[key] = this.position[key]);
+        document.body.appendChild(container);
+  
+        const questionElement = document.createElement('div');
+        questionElement.textContent = 'How likely are you to recommend us to a friend or colleague?';
+        container.appendChild(questionElement);
+  
+        const scaleElement = document.createElement('div');
+        for (let i = 0; i <= 10; i++) {
+          const numberElement = document.createElement('button');
+          numberElement.textContent = i;
+          numberElement.style.backgroundColor = i === 0 ? '#ff6666' : i === 10 ? '#66cc66' : '#ffffff';
+          scaleElement.appendChild(numberElement);
+        }
+        container.appendChild(scaleElement);
+      }
     }
-  }
   
-  // Attach Widget class to the global scope
-  window.Widget = Widget;
-  
+    // Attach the Widget class to the global window object
+    window.Widget = Widget;
+  })();
